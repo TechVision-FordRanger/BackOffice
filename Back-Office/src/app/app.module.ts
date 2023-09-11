218021a5104cfe16c8b1ffd45896e993498dd4a8
Jéssica Zibini <jessicazibinia@gmail.com>
2023-09-11 17:53:54 -0300

    Instalação do Angular e Bootstrap e criaçao do Menu

---
+++ Back-Office/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuLateralComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
