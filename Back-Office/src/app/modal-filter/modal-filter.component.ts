e3c29236f665f6650768cdb5b3d2530c90583126
Gambeti <Gambeti.KG@gmail.com>
2023-09-29 22:02:04 -0300

    FEAT: foi implementado um filtro para o modal de filtros e uma validação caso não preencham o primeiro campo.

--- Back-Office/src/app/modal-filter/modal-filter.component.ts
+++ Back-Office/src/app/modal-filter/modal-filter.component.ts
@@ -1,19 +1,43 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {BsModalRef} from "ngx-bootstrap/modal";
+import {Component, EventEmitter, Output} from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-modal-filter',
   templateUrl: './modal-filter.component.html',
   styleUrls: ['./modal-filter.component.css']
 })
+
 export class ModalFilterComponent {
-  @Input() title = 'filtros';
+  selectedCountry: string = '';
+  selectedState: string = '';
+  selectedDate: string = '';
+
+  @Output() closeEmitter = new EventEmitter();
   @Output() confirmEmitter =  new EventEmitter();
-constructor(public bsModalRef: BsModalRef) {
+  constructor(public bsModalRef: BsModalRef) {}
+
+
+  confirmFilter() {
+    if (!this.selectedCountry || !this.selectedState) {
+      alert('Por favor, selecione um país e um estado antes de confirmar.');
+      return;
+    }
+
+    this.confirmEmitter.emit({
+      country: this.selectedCountry,
+      state: this.selectedState,
+      date: this.selectedDate
+    });
+
+    this.bsModalRef.hide();
+  }
+
+  closeEvent(){
+    this.confirmEmitter.emit();
+  }
+
 }
 
-confirmFilter(){
-  this.confirmEmitter.emit();
-}
 
-}
+
+
